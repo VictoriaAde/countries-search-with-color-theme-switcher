c2faf8e5733f1f2b5a6408b6ad2cf6a9b099d75c
Victoria <adedayovicky123@gmail.com>
2023-08-21 16:49:52 +0100

    create Html structure of page, add css variable

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,34 @@ import "./App.css";
 
 function App() {
   return (
-    <div>
-      <h1>Contries</h1>
-    </div>
+    <main>
+      <div>
+        <div>
+          <h1>Where in the world?</h1>
+        </div>
+        <div>
+          <span>Dark mode</span>
+        </div>
+      </div>
+
+      <div>
+        <input
+          type="search"
+          name=""
+          id=""
+          placeholder="Search for country..."
+        />
+      </div>
+
+      <div>dropdown</div>
+
+      <div>
+        <h3>United States of America</h3>
+        <p>Population: 345.66.000</p>
+        <p>Region: Americas</p>
+        <p>Capital: Washinton D.C</p>
+      </div>
+    </main>
   );
 }
 
